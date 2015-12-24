0d937ed9160f35d842de0fc568f9b97fcc3dcb04
lgilone <lgilone@devartis.com>
2015-12-24 11:20:46 -0300

    Initial commit

---
+++ app/src/main/java/com/prueba/lucas/prueba1/MainActivity.java
@@ -0,0 +1,113 @@
+package com.prueba.lucas.prueba1;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+public class MainActivity extends ActionBarActivity {
+    Button b1,b2,b3,b4,b5,b6,b7,b8;
+
+    public final static String EXTRA_MESSAGE = "com.prueba.lucas.prueba1.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1 =(Button) findViewById(R.id.button1);
+        b2= (Button) findViewById(R.id.button2);
+        b3= (Button) findViewById(R.id.button3);
+        b4= (Button) findViewById(R.id.button4);
+        b5= (Button) findViewById(R.id.button5);
+        b6= (Button) findViewById(R.id.button6);
+        b7= (Button) findViewById(R.id.button7);
+        b8= (Button) findViewById(R.id.button8);
+        this.createListeners();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void createListeners(){
+        b2.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(2);
+            }
+        });
+        b3.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(3);
+            }
+        });
+        b4.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(4);
+            }
+        });
+        b1.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(1);
+            }
+        });
+        b5.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(5);
+            }
+        });
+        b6.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(6);
+            }
+        });
+        b7.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(7);
+            }
+        });
+        b8.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                startSecondActivity(8);
+            }
+        });
+    }
+
+    private void startSecondActivity(int buttonNum) {
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra("BUTTON NUMBER", buttonNum);
+        startActivity(intent);
+    }
+
+    public void sendMessage(View view){
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        //EditText editText = (EditText) findViewById(R.id.edit_message);
+        //String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, "Ehh wacho.");
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/prueba/lucas/prueba1/SecondActivity.java
@@ -0,0 +1,107 @@
+package com.prueba.lucas.prueba1;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class SecondActivity extends ActionBarActivity {
+    private MediaPlayer mPlayer;
+    private int currentSong = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+        int number = intent.getIntExtra("BUTTON NUMBER", 1);
+
+        /*TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText(String.valueOf(number));*/
+
+        if (number == 1) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.nestoresta);
+            currentSong = R.raw.nestoresta;
+        } else if (number == 2) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.nestormorir);
+            currentSong = R.raw.nestormorir;
+
+        } else if (number == 3) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.como);
+            currentSong = R.raw.como;
+
+        } else if (number == 4) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.pordios);
+            currentSong = R.raw.pordios;
+        } else if (number== 5) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.perosenor);
+            currentSong = R.raw.perosenor;
+        }  else if (number== 6) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.comoprogresaste );
+            currentSong = R.raw.comoprogresaste;
+        } else if (number== 7) {
+            mPlayer= MediaPlayer.create(SecondActivity.this,R.raw.ricaamarga);
+            currentSong=R.raw.ricaamarga;
+            // Defaults are always a good idea:
+        }   else if (number== 8) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.hablesenor);
+            currentSong = R.raw.hablesenor;
+            // Defaults are always a good idea:
+        } else {
+            mPlayer = MediaPlayer.create(SecondActivity.this, R.raw.como);
+            currentSong = R.raw.como;
+
+        }
+
+        mPlayer.start();
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if (mPlayer.isPlaying()) {
+            mPlayer.pause();
+        }
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mPlayer == null) {
+            mPlayer = MediaPlayer.create(SecondActivity.this, currentSong);
+        }
+
+        mPlayer.start();
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mPlayer.isPlaying()) {
+            mPlayer.stop();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
