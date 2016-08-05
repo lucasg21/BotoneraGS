a192e975f1422043aa315c96d719bef4fcc692b8
lgilone <lgilone@devartis.com>
2016-08-04 23:48:13 -0300

    Play found sounds + ads in results view

--- botonerags/src/main/java/com/lucassaka/botonerags/SearchableActivity.java
+++ botonerags/src/main/java/com/lucassaka/botonerags/SearchableActivity.java
@@ -6,6 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 
 import java.util.ArrayList;
 
@@ -13,13 +17,25 @@ import java.util.ArrayList;
  * Created by Lucas on 30/07/2016.
  */
 public class SearchableActivity extends ListActivity {
-    ArrayAdapter<String> adapter;
-    ArrayList<String> listItems=new ArrayList<String>();
+    ArrayList<Sound> listSounds = new ArrayList<>();
+    ArrayAdapter<Sound> soundAdapter;
+    private SoundPlayer mSoundPlayer;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.searchresults);
+
+        mSoundPlayer = new SoundPlayer(this.getApplicationContext());
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("395F39CE8F56D34CBE41785D5F12B2C7")
+                .addTestDevice("7C0CAD91C1F4E71ABF3D62F98FCD84D7") //S6 6.0 en Genymotion
+                .build();
+
+        mAdView.loadAd(adRequest);
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
@@ -30,10 +46,8 @@ public class SearchableActivity extends ListActivity {
     }
 
     private void doMySearch(String query) {
-        adapter=new ArrayAdapter<String>(this,
-                R.layout.resultitem,R.id.nombreSonido,
-                listItems);
-        setListAdapter(adapter);
+        soundAdapter= new ArrayAdapter<>(this,R.layout.resultitem,R.id.nombreSonido,listSounds);
+        setListAdapter(soundAdapter);
 
         final Sound[] soundArray = SoundStore.getSounds(this.getApplicationContext());
 
@@ -41,9 +55,14 @@ public class SearchableActivity extends ListActivity {
             boolean containsSound;
             containsSound=soundArray[i].getName().toLowerCase().contains(query.toLowerCase());
             if(containsSound){
-                listItems.add(soundArray[i].getName());
+                listSounds.add(soundArray[i]);
             }
         }
-        adapter.notifyDataSetChanged();
+        soundAdapter.notifyDataSetChanged();
+    }
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Sound selectedSound = (Sound) getListAdapter().getItem(position);
+        mSoundPlayer.playSound(selectedSound);
     }
 }
